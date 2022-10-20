1be8f6ddf64120d0f5b80a510f8678c5d79cd66c
MattRuetz <matt.b.ruetz@gmail.com>
2022-10-20 11:49:07 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import './scripts/scroll-timeline.js';
+// import initScrollReveal from './scripts/scrollReveal.js';
+import initTiltEffect from './scripts/tiltAnimation.js';
+// import { targetElements, defaultProps } from './data/scrollRevealConfig';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
+
+// initScrollReveal(targetElements, defaultProps);
+initTiltEffect((res) => {
+    console.log(res);
+});
+
+let logo = document.getElementById('logo');
+
+// Flip logo to white after 500ms
+function invertLogo() {
+    setTimeout(() => {
+        logo.classList.add('invert');
+    }, 500);
+}
+
+invertLogo();
