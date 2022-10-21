297b47b06a10cb186e1b427d186804a3894cf0b7
MattRuetz <matt.b.ruetz@gmail.com>
2022-10-20 21:48:31 -0400

    figured out on-scroll animation for thru-line design element

--- src/scripts/GSAP_data.js
+++ src/scripts/GSAP_data.js
@@ -2,3 +2,19 @@
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
+
+// SCROLL TRIGGERS
+
+// Thru line vertical down
+gsap.to('.thru-line', {
+    scale: 1,
+    y: 900,
+    ease: 'expo',
+    scrollTrigger: {
+        markers: true,
+        start: 'bottom top',
+        end: '1500px top',
+        trigger: '.thru-line',
+        scrub: 3,
+    },
+});
